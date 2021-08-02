9672d1c23b202a8d971c4df631f87e084a7cd13f
ShamrokhMuhammad <MuhammadSh@mta.ac.il>
2021-08-02 15:00:25 +0300

    After DAYTIMEORIENTED instance in enum crossover class

--- LogicEngine/src/AlgorithmClasses/Crossover.java
+++ LogicEngine/src/AlgorithmClasses/Crossover.java
@@ -1,19 +1,25 @@
 package AlgorithmClasses;
 
+import DataClasses.AlgorithmData.Generation;
+import DataClasses.AlgorithmData.Parent;
 import DataTransferClasses.CrossoverData;
 import ParsedClasses.ETTCrossover;
 
+import java.util.Locale;
+
 public class Crossover {
 
-    private String m_Configuration;
     private String m_Name;
     private Integer m_CuttingPoints;
+    private eCrossover m_Configuration;
+    private Generation m_NewGeneration;
 
     public Crossover(ETTCrossover i_ETTCrossover)
     {
-        m_Configuration=i_ETTCrossover.getConfiguration();
         m_Name=i_ETTCrossover.getName();
         m_CuttingPoints=i_ETTCrossover.getCuttingPoints();
+        m_Configuration=eCrossover.valueOf(m_Name.toUpperCase(Locale.ROOT));
+        m_NewGeneration=new Generation();
     }
 
     public CrossoverData getCrossoverData()
@@ -21,7 +27,7 @@ public class Crossover {
         return new CrossoverData(this);
     }
 
-    public String getConfiguration() {
+    public eCrossover getConfiguration() {
         return m_Configuration;
     }
 
@@ -31,5 +37,10 @@ public class Crossover {
 
     public Integer getCuttingPoints() {
         return m_CuttingPoints;
+    }
+
+    public void addToNewGeneration(Parent i_Parent)
+    {
+        m_NewGeneration.addSolutionToList(i_Parent);
     }
 }
