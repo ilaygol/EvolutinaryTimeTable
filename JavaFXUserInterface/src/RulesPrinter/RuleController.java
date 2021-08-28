5564d0f64f4ba30104cea241d47ffbd6b05b9419
Ilay Gol <ilay-g@hotmail.com>
2021-08-28 15:25:46 +0300

    Rule printer component

---
+++ JavaFXUserInterface/src/RulesPrinter/RuleController.java
@@ -0,0 +1,21 @@
+package RulesPrinter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class RuleController {
+
+    @FXML
+    private Text ruleNameText;
+
+    @FXML
+    private Label ruleTypeLabel;
+
+    @FXML
+    private Label ruleParamLabel;
+
+    @FXML
+    private Label ruleScoreLabel;
+
+}
