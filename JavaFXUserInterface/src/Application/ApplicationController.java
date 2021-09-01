bf1c67a52ec9a9ddeeca687c1a96f68399ce300a
Ilay Gol <ilay-g@hotmail.com>
2021-09-01 10:39:05 +0300

    controller imports

--- JavaFXUserInterface/src/Application/ApplicationController.java
+++ JavaFXUserInterface/src/Application/ApplicationController.java
@@ -10,12 +10,9 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
