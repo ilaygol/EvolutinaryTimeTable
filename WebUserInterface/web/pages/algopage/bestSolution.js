861e118efb66b9caa72a65649c369f8246949cfd
Ilay Gol <ilay-g@hotmail.com>
2021-09-26 21:40:21 +0300

    raw best solution done

--- WebUserInterface/web/pages/algopage/bestSolution.js
+++ WebUserInterface/web/pages/algopage/bestSolution.js
@@ -12,7 +12,16 @@ $(function(){
                   url:"rawSolution",
                   timeout:2000,
                   success: function(webLessonsList){
-                      //ilay
+                      $("<div id='rawSolutionContent' class='d-flex flex-row flex-wrap justify-content-between p-3 gap-1'></div>").appendTo($("#bestSolutionContent").empty());
+                      $.each(webLessonsList || [],function (index,lesson) {
+                          $("<table class='table table-sm table-striped w-auto text-center align-middle table-bordered border'>" +
+                              "<tbody><tr><td>" + "Day" + "</td><td>" + lesson["m_Day"] + "</td></tr>" +
+                              "<tr><td>" + "Hour" + "</td><td>" + lesson["m_Hour"] + "</td></tr>" +
+                              "<tr><td>" + "Class" + "</td><td>" + lesson["m_ClassName"] + "</td></tr>" +
+                              "<tr><td>" + "Teacher" + "</td><td>" + lesson["m_TeacherName"] + "</td></tr>" +
+                              "<tr><td>" + "Subject" + "</td><td>" + lesson["m_SubjectName"] + "</td></tr>" +
+                              "</tbody></table>").appendTo($("#rawSolutionContent"));
+                      });
                   },
                   error:function(errorObj){
                       var myModal = new bootstrap.Modal(document.getElementById('algoRefModal'));
@@ -30,6 +39,7 @@ $(function(){
                   success:function (teachersList){
                      //m_TeacherID ---> teacher id member
                      //m_TeacherName ---> teacher name member
+
                   },
                   error:function (){
                       console.log("failed to get Teachers id names list")
